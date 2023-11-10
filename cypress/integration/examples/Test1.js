d6902b2d0b898a313752eab60a513dc808b2b1d6
Damien <damien.teo.ks@gmail.com>
2023-11-10 15:03:37 +0800

    Add first test

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,31 @@
+/// <reference types='Cypress' />
+
+describe("First Test suite", () => {
+  it("first test case", () => {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get(".search-keyword").type("ca");
+    // cy.get(".product:visible").should("have.length", 4);
+    cy.get(".products").find(".product").should("have.length", 4);
+
+    cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click(); // find the element
+
+    cy.get(".products")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const text = $el.find(".product-name").text();
+        if (text.includes("Cashews")) {
+          cy.wrap($el).find("button").click();
+          //   $el.contains("ADD TO CART").click();
+        }
+      });
+
+    cy.get(".products").find(".product").as("productsFound");
+    cy.get("@productsFound").should("have.length", 4);
+
+    cy.get(".brand").should("have.text", "GREENKART");
+
+    cy.get(".brand").then((logoelement) => {
+      cy.log(logoelement.text());
+    });
+  });
+});
