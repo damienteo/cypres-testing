fcf9e126377f5cd47f5272757899f87d7353003a
Damien <damien.teo.ks@gmail.com>
2023-11-16 11:47:54 +0800

    Use fixtures json  and support commands

--- cypress/integration/examples/Test7.js
+++ cypress/integration/examples/Test7.js
@@ -3,11 +3,39 @@
 import "cypress-iframe";
 
 describe("Seventh Test suite", () => {
-  it("should be able to test frameworks with hooks", () => {
+  before(() => {
+    cy.fixture("example").then(function (data) {
+      this.data = data;
+    });
+  });
+
+  it("should be able to test frameworks with hooks", function () {
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
 
-    cy.get("input[name='name']:nth-child(1").type("Bob");
+    cy.get("input[name='name']:nth-child(2)").type(this.data.name);
 
-    cy.get("select").select("Female");
+    cy.get("select").select(this.data.gender);
+
+    cy.get(":nth-child(4) > .ng-untouched").should(
+      "have.value",
+      this.data.name
+    );
+
+    cy.get("input[name='name']:nth-child(2)").should(
+      "have.attr",
+      "minlength",
+      "2"
+    );
+
+    cy.get("#inlineRadio3").should("be.disabled");
+
+    cy.get(":nth-child(2) > .nav-link").click();
+
+    // cy.get("h4.card-title").each(($el, index, $list) => {
+    //   if ($el.text().includes("Blackberry")) {
+    //     cy.get("button.btn.btn-info").eq(index).click();
+    //   }
+    // });
+    cy.selectProduct("Blackberry");
   });
 });
