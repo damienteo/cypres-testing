ad627a11e944300f2535803d5887c022d967724c
Damien <damien.teo.ks@gmail.com>
2023-11-16 16:23:50 +0800

    E2E test of business flow

--- cypress/integration/examples/Test7.js
+++ cypress/integration/examples/Test7.js
@@ -41,5 +41,18 @@ describe("Seventh Test suite", () => {
     });
 
     productPage.checkoutButton().click();
+    cy.contains("Checkout").click();
+    cy.get("#country").type("India");
+    cy.get(".suggestions > ul > li > a").click();
+    cy.get("#checkbox2").click({ force: true });
+    cy.get('input[type="submit"]').click();
+    // cy.get(".alert").should(
+    //   "have.text",
+    //   "Success! Thank you! Your order will be delivered in next few weeks :-)."
+    // );
+    cy.get(".alert").then(function (element) {
+      const actualtext = element.text();
+      expect(actualtext.includes("Success")).to.be.true;
+    });
   });
 });
