fc47439b27d88615631c4a986cc32fc102b26625
Damien <damien.teo.ks@gmail.com>
2023-11-16 14:00:34 +0800

    Add page objects

--- cypress/integration/examples/Test7.js
+++ cypress/integration/examples/Test7.js
@@ -1,6 +1,7 @@
 /// <reference types='Cypress' />
-/// <reference types='cypress-iframe' />
-import "cypress-iframe";
+
+import HomePage from "../pageObjects/HomePage";
+import ProductPage from "../pageObjects/ProductPage";
 
 describe("Seventh Test suite", () => {
   before(() => {
@@ -10,32 +11,35 @@ describe("Seventh Test suite", () => {
   });
 
   it("should be able to test frameworks with hooks", function () {
+    const homePage = new HomePage();
+    const productPage = new ProductPage();
+
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
 
-    cy.get("input[name='name']:nth-child(2)").type(this.data.name);
+    homePage.getEditBox().type(this.data.name);
 
-    cy.get("select").select(this.data.gender);
+    homePage.getGender().select(this.data.gender);
 
-    cy.get(":nth-child(4) > .ng-untouched").should(
-      "have.value",
-      this.data.name
-    );
+    homePage.getTwoWayDataBinding().should("have.value", this.data.name);
 
-    cy.get("input[name='name']:nth-child(2)").should(
-      "have.attr",
-      "minlength",
-      "2"
-    );
+    homePage.getEditBox().should("have.attr", "minlength", "2");
 
-    cy.get("#inlineRadio3").should("be.disabled");
+    homePage.getEntrepreneur().should("be.disabled");
 
-    cy.get(":nth-child(2) > .nav-link").click();
+    homePage.getShopTab().click();
 
     // cy.get("h4.card-title").each(($el, index, $list) => {
     //   if ($el.text().includes("Blackberry")) {
     //     cy.get("button.btn.btn-info").eq(index).click();
     //   }
     // });
-    cy.selectProduct("Blackberry");
+    // cy.selectProduct(this.data.productName[0]);
+    // cy.selectProduct(this.data.productName[1]);
+
+    this.data.productName.forEach((name) => {
+      cy.selectProduct(name);
+    });
+
+    productPage.checkoutButton().click();
   });
 });
