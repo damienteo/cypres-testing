0e35c3919da93cde51215e028411d6c68b25a637
Damien <damien.teo.ks@gmail.com>
2023-11-17 11:17:03 +0800

    Add scripts

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,9 @@ module.exports = defineConfig({
   env: {
     url: "https://rahulshettyacademy.com",
   },
+  retries: {
+    runMode: 1,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
