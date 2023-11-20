29d1e689d1f7687658e5841bfcce9e5f9b2177b7
Damien <damien.teo.ks@gmail.com>
2023-11-20 11:19:50 +0800

    Add e2e test

--- cypress/integration/examples/sessionTest.js
+++ cypress/integration/examples/sessionTest.js
@@ -10,5 +10,18 @@ describe("Session Test suite", () => {
         },
       });
     });
+
+    cy.get(".card-body button:last-of-type").eq(1).click();
+    cy.get("[routerlink*='cart']").click();
+    cy.contains("Checkout").click();
+    cy.get("[placeholder*='Country").type("ind");
+    cy.get(".ta-results button").each(($el, index, $list) => {
+      if ($el.text() === " India") {
+        cy.wrap($el).click();
+      }
+    });
+    cy.get(".action__submit").click();
+    cy.wait(2000);
+    cy.get(".order-summary button").eq(0).click();
   });
 });
